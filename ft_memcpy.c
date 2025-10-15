5da87f99bd893b2d4236f731539098dfde3190ee
Hikaru Wakatsuki <hwakatsu@c2r3s9.42tokyo.jp>
2025-10-15 22:12:30 +0900

    waka

--- ft_memcpy.c
+++ ft_memcpy.c
@@ -6,13 +6,13 @@
 /*   By: hwakatsu <hwakatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 21:16:29 by hwakatsu          #+#    #+#             */
-/*   Updated: 2025/10/15 22:00:12 by hwakatsu         ###   ########.fr       */
+/*   Updated: 2025/10/15 22:12:17 by hwakatsu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*memcpy(void *dest, const void *src, size_t n)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
 {
 	size_t			i;
 	unsigned char	*dest2;
@@ -26,4 +26,16 @@ void	*memcpy(void *dest, const void *src, size_t n)
 		dest2[i] = src2[i];
 		i++;
 	}
+	return (dest);
 }
+
+// #include <stdio.h>
+
+// int	main()
+// {
+// 	char src[] = "Hello";
+// 	char dest[] = "12345";
+
+// 	ft_memcpy(dest, src, 3);
+// 	printf("%s", dest);
+// }
