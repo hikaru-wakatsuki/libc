92b9d1fd94ed37824e0622c748966779ab663ff9
mac <waka9648hika46@gmail.com>
2025-10-15 06:47:52 +0000

    waka

---
+++ ft_isascii.c
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_isascii.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hwakatsu <hwakatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/15 06:39:14 by hwakatsu          #+#    #+#             */
+/*   Updated: 2025/10/15 06:47:34 by hwakatsu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+int	ft_isascii(int c)
+{
+	if ((0 <= c && c <= 127))
+		return (1);
+	return (0);
+}
+
+//#include <stdio.h>
+//#include <ctype.h>
+
+//int	main(void)
+//{
+//	char c = '0';
+//	printf("%c\nisascii = %d\n", c, isascii(c));
+//	printf("ft_isascii = %d\n", ft_isascii(c));
+//}
