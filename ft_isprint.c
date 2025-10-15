cf36e0d63726b826212791d76148794398ffd289
mac <waka9648hika46@gmail.com>
2025-10-15 07:07:02 +0000

    waka

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,7 +6,7 @@
 /*   By: hwakatsu <hwakatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 06:49:21 by hwakatsu          #+#    #+#             */
-/*   Updated: 2025/10/15 06:50:47 by hwakatsu         ###   ########.fr       */
+/*   Updated: 2025/10/15 07:06:47 by hwakatsu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,17 @@
 
 int	ft_isprint(int c)
 {
-
+	if (32 <= c && c <= 126)
+		return (16384);
+	return (0);
 }
+
+//#include <stdio.h>
+//#include <ctype.h>
+
+//int	main(void)
+//{
+//	char c = 'a';
+//	printf("%c\nisprint = %d\n", c, isprint(c));
+//	printf("ft_isprint = %d\n", ft_isprint(c));
+//}
