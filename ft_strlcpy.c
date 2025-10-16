7cfcfc83dbb3bf9191c23a18ed07838e1a1e5d94
mac <waka9648hika46@gmail.com>
2025-10-16 08:07:17 +0000

    waka

--- ft_strlcpy.c
+++ ft_strlcpy.c
@@ -6,86 +6,39 @@
 /*   By: hwakatsu <hwakatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 06:41:50 by hwakatsu          #+#    #+#             */
-/*   Updated: 2025/10/16 15:50:27 by hwakatsu         ###   ########.fr       */
+/*   Updated: 2025/10/16 08:06:44 by hwakatsu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
+size_t    ft_strlcpy(char *dest, const char *src, size_t size)
+{
+     size_t    i;
+     size_t    src_len;
 
+     src_len = ft_strlen(src);
+     i = 0;
+     if (size != 0)
+     {
+          while (i < size - 1 && src[i])
+          {
+               dest[i] = src[i];
+               i++;
+          }
+          dest[i] = '\0';
+     }
+     return(src_len);
+}
 
-STRLCPY(3bsd)                        LOCAL                       STRLCPY(3bsd)
+#include <stdio.h>
 
-NAME
-     strlcpy, strlcat — size-bounded string copying and concatenation
+int  main()
+{
+     char dest[] = "Hello";
+     char src[] = "12345";
 
-LIBRARY
-     Utility functions from BSD systems (libbsd, -lbsd)
+     printf("%ld\n", ft_strlcpy(dest, src, 5));
+     printf("%s\n", dest);
 
-SYNOPSIS
-     #include <string.h>
-     (See libbsd(7) for include usage.)
-
-     size_t
-     strlcpy(char *dst, const char *src, size_t size);
-
-     size_t
-     strlcat(char *dst, const char *src, size_t size);
-
-DESCRIPTION
-     The strlcpy() and strlcat() functions copy and concatenate strings re‐
-     spectively.  They are designed to be safer, more consistent, and less er‐
-     ror prone replacements for strncpy(3) and strncat(3).  Unlike those func‐
-     tions, strlcpy() and strlcat() take the full size of the buffer (not just
-     the length) and guarantee to NUL-terminate the result (as long as size is
-     larger than 0 or, in the case of strlcat(), as long as there is at least
-     one byte free in dst).  Note that a byte for the NUL should be included
-     in size.  Also note that strlcpy() and strlcat() only operate on true “C”
-     strings.  This means that for strlcpy() src must be NUL-terminated and
-     for strlcat() both src and dst must be NUL-terminated.
-
-     The strlcpy() function copies up to size - 1 characters from the NUL-ter‐
-     minated string src to dst, NUL-terminating the result.
-
-     The strlcat() function appends the NUL-terminated string src to the end
-     of dst.  It will append at most size - strlen(dst) - 1 bytes, NUL-termi‐
-     nating the result.
-
-RETURN VALUES
-     The strlcpy() and strlcat() functions return the total length of the
-     string they tried to create.  For strlcpy() that means the length of src.
-     For strlcat() that means the initial length of dst plus the length of
-     src.  While this may seem somewhat confusing, it was done to make trunca‐
-     tion detection simple.
-
-     Note, however, that if strlcat() traverses size characters without find‐
-     ing a NUL, the length of the string is considered to be size and the des‐
-     tination string will not be NUL-terminated (since there was no space for
-     the NUL).  This keeps strlcat() from running off the end of a string.  In
-     practice this should not happen (as it means that either size is incor‐
-     rect or that dst is not a proper “C” string).  The check exists to pre‐
-     vent potential security problems in incorrect code.
-
-EXAMPLES
-     The following code fragment illustrates the simple case:
-
-           char *s, *p, buf[BUFSIZ];
-
-           ...
-
-           (void)strlcpy(buf, s, sizeof(buf));
-           (void)strlcat(buf, p, sizeof(buf));
-
-     To detect truncation, perhaps while building a pathname, something like
-     the following might be used:
-
-           char *dir, *file, pname[MAXPATHLEN];
-
-           ...
-
-           if (strlcpy(pname, dir, sizeof(pname)) >= sizeof(pname))
-                   goto toolong;
-           if (strlcat(pname, file, sizeof(pname)) >= sizeof(pname))
-                   goto toolong;
-
-     Since it is known how many characters were copied the first time, things
+}
