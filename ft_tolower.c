f5f0894df5983053050eef5608f1b1b178414bb0
Hikaru Wakatsuki <hwakatsu@c1r1s7.42tokyo.jp>
2025-10-16 20:39:16 +0900

    waka

--- ft_tolower.c
+++ ft_tolower.c
@@ -6,7 +6,25 @@
 /*   By: hwakatsu <hwakatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 19:59:29 by hwakatsu          #+#    #+#             */
-/*   Updated: 2025/10/16 19:59:30 by hwakatsu         ###   ########.fr       */
+/*   Updated: 2025/10/16 20:38:50 by hwakatsu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
+int	ft_tolower(int c)
+{
+	if (65 <= c && c <= 90)
+		return (c + 32);
+	return (c);
+}
+
+// #include <stdio.h>
+// #include <ctype.h>
+
+// int	main(void)
+// {
+// 	char c = 'A';
+// 	printf("%c\ntolower = %c\n", c, (char)tolower(c));
+// 	printf("ft_tolower = %c\n", (char)ft_tolower(c));
+// }
--- ft_toupper.c
+++ ft_toupper.c
@@ -6,7 +6,7 @@
 /*   By: hwakatsu <hwakatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 19:24:43 by hwakatsu          #+#    #+#             */
-/*   Updated: 2025/10/16 19:47:28 by hwakatsu         ###   ########.fr       */
+/*   Updated: 2025/10/16 20:39:03 by hwakatsu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,12 +19,12 @@ int	ft_toupper(int c)
 	return (c);
 }
 
-#include <stdio.h>
-#include <ctype.h>
+// #include <stdio.h>
+// #include <ctype.h>
 
-int	main(void)
-{
-	char c = 'a';
-	printf("%c\ntoupper = %d\n", c, toupper(c));
-	printf("ft_toupper = %d\n", ft_toupper(c));
-}
+// int	main(void)
+// {
+// 	char c = 'a';
+// 	printf("%c\ntoupper = %c\n", c, (char)toupper(c));
+// 	printf("ft_toupper = %c\n", (char)ft_toupper(c));
+// }
