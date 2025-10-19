9874993b5b1ce070e36370d1b22bb17fcba9d495
Hikaru Wakatsuki <hwakatsu@c5r2s6.42tokyo.jp>
2025-10-19 15:13:33 +0900

    waka

---
+++ ft_strmapi.c
@@ -0,0 +1,48 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strmapi.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: hwakatsu <hwakatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/10/19 14:41:17 by hwakatsu          #+#    #+#             */
+/*   Updated: 2025/10/19 15:11:40 by hwakatsu         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char))
+{
+	char			*map;
+	unsigned int	index;
+	size_t			s_len;
+
+	s_len = strlen(s);
+	map = (char *)malloc(sizeof(char) * (s_len + 1));
+	if (!map)
+		return (NULL);
+	index = 0;
+	while (s[index])
+	{
+		map[index] = f(index, s[index]);
+		index++;
+	}
+	map[index] = '\0';
+	return (map);
+}
+
+// char	under_second_tolower(unsigned int i, char c)
+// {
+// 	if (i != 0)
+// 		return ((char)ft_tolower((int)c));
+// 	return (c);
+// }
+
+// #include <stdio.h>
+
+// int	main(void)
+// {
+// 	char s[] = "HELLO";
+// 	printf("ft_strmapi = %s\n", ft_strmapi(s, under_second_tolower));
+// }
