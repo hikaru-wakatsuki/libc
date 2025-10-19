3e506fe71f79cfcc0e8c074b0aa1a2764250f4de
Hikaru Wakatsuki <hwakatsu@c5r2s6.42tokyo.jp>
2025-10-19 16:07:59 +0900

    waka

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: hwakatsu <hwakatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/16 21:28:58 by hwakatsu          #+#    #+#             */
-/*   Updated: 2025/10/19 14:14:52 by hwakatsu         ###   ########.fr       */
+/*   Updated: 2025/10/19 16:07:26 by hwakatsu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,28 +14,32 @@
 
 int	ft_strncmp(const char *s1, const char *s2, size_t n)
 {
-	size_t	i;
+	size_t				i;
+	unsigned const char	*us1;
+	unsigned const char	*us2;
 
-	i = 0;
+	us1 = (unsigned const char *)s1;
+	us2 = (unsigned const char *)s2;
 	if (n == 0)
 		return (0);
-	while (i + 1 < n && s1[i])
+	i = 0;
+	while (i + 1 < n && us1[i])
 	{
-		if (s1[i] != s2[i])
-			return (s1[i] - s2[i]);
+		if (us1[i] != us2[i])
+			return ((int)(us1[i] - us2[i]));
 		i++;
 	}
-	return (s1[i] - s2[i]);
+	return ((int)(us1[i] - us2[i]));
 }
 
 // #include <stdio.h>
 
 // int	main(void)
 // {
-// 	char	s1[] = "abcd";
-// 	char	s2[] = "abc";
+// 	char	s1[] = "\200";
+// 	char	s2[] = "\0";
 
-// 	printf("%d\n", ft_strncmp(s1, s2, 4));
-// 	printf("%d\n", strncmp(s1, s2, 4));
+// 	printf("%d\n", ft_strncmp(s1, s2, 1));
+// 	printf("%d\n", strncmp(s1, s2, 1));
 // 	return (0);
 // }
