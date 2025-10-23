7ab0e6b1795fb3c48b513834e988efe44f04071e
Hikaru Wakatsuki <hwakatsu@c5r3s11.42tokyo.jp>
2025-10-23 21:34:42 +0900

    waka

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,15 +6,16 @@
 /*   By: hwakatsu <hwakatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 03:44:45 by hwakatsu          #+#    #+#             */
-/*   Updated: 2025/10/20 18:45:27 by hwakatsu         ###   ########.fr       */
+/*   Updated: 2025/10/23 21:31:23 by hwakatsu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft.h"
+
 int	ft_isalnum(int c)
 {
-	if (('a' <= c && c <= 'z') || ('A' <= c && c <= 'Z')
-		|| ('0' <= c && c <= '9'))
-		return (8);
+	if (ft_isalpha(c) || ft_isdigit(c))
+		return (1);
 	return (0);
 }
 
--- ft_isascii.c
+++ ft_isascii.c
@@ -6,13 +6,13 @@
 /*   By: hwakatsu <hwakatsu@student.42tokyo.jp>     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/10/15 06:39:14 by hwakatsu          #+#    #+#             */
-/*   Updated: 2025/10/20 18:45:40 by hwakatsu         ###   ########.fr       */
+/*   Updated: 2025/10/23 21:32:36 by hwakatsu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 int	ft_isascii(int c)
 {
-	if ((0 <= c && c <= 127))
+	if (0 <= c && c <= 127)
 		return (1);
 	return (0);
 }
